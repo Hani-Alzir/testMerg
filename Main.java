11a80c7ec9c9cc50ed901657ebc5b4a545e955a2
hani.alzir <hani.alzir@telavox.com>
2023-03-30 15:17:53 +0200

    sadcsccsweew

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("show me");
-        System.out.println("css");
+        System.out.println("cssss");
 
 
 
