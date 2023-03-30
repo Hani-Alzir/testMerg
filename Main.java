856083da0e13a7fd8bb23c4efff473991bfcb42d
hani.alzir <hani.alzir@telavox.com>
2023-03-30 14:30:32 +0200

    lets try3

--- Main.java
+++ Main.java
@@ -1,9 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("5");
-        System.out.println("saccs");
-        System.out.println("ccs");
+
         System.out.println("show me");
 
 
