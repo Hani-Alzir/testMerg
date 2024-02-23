604685e9f331a08b510ad0f30f59ebbf25d70793
Hani Alzir <122264536+Hani-Alzir@users.noreply.github.com>
2024-02-23 13:47:53 +0100

    test

--- Main.java
+++ Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("show me");
-        System.out.println("cssss");
+
 
 
 
