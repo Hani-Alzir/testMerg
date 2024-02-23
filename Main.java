39835e8909a4396a866d7da2a3caf1c736078ebd
Hani Alzir <122264536+Hani-Alzir@users.noreply.github.com>
2024-02-23 14:42:54 +0100

    asdwe

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
+        for (int i = 0; i < 50; i++){
+            System.out.println(i);
+        }
 
     }
 }
